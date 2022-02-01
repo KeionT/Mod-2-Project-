ffdb1147e64c264697893f9cbbc9968b8a9d625c
KeionT <93876343+KeionT@users.noreply.github.com>
2022-02-01 08:55:01 -0500

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.scss'
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Food <span>Buddy</span> &copy;</p>
+        </div>
+    )
+}
+
+export default Footer
